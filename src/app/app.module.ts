fd48bfa9b4c12f619f27ec1bdc91cf4d48520716
Jose Horacio <jhoracio19@hotmail.com>
2025-03-30 22:27:24 -0600

    Iniciando proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
