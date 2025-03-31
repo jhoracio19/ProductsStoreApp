3ef78c666161d5caa3b5c25c3aecc91f676441c3
Jose Horacio <jhoracio19@hotmail.com>
2025-03-30 22:41:54 -0600

    agregando products-details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  productId!: number;
+  product: any;
+
+  products = [
+    { id: 1, name: 'Producto 1', description: 'Descripción detallada del Producto 1' },
+    { id: 2, name: 'Producto 2', description: 'Descripción detallada del Producto 2' },
+    { id: 3, name: 'Producto 3', description: 'Descripción detallada del Producto 3' },
+  ];
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.productId = +this.route.snapshot.params['id'];
+    this.product = this.products.find(p => p.id === this.productId);
+  }
 
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products = [
+    { id: 1, name: 'Producto 1', description: 'Descripción corta del Producto 1' },
+    { id: 2, name: 'Producto 2', description: 'Descripción corta del Producto 2' },
+    { id: 3, name: 'Producto 3', description: 'Descripción corta del Producto 3' },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
 
 }
